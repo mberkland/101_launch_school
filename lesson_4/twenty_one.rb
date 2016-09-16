505c30307f5e14ea16b3b2f589dcd8efe5c8ee2b
mberkland <miruke0506@hotmail.com>
2016-09-16 15:53:25 +0000

    revisions to 21 game

--- lesson_4/twenty_one.rb
+++ lesson_4/twenty_one.rb
@@ -6,49 +6,48 @@ STRAIGHT = [["Ace", 1], ["2", 2], ["3", 3], ["4", 4], ["5", 5], ["6", 6],
             ["Queen", 10], ["King", 10]].freeze
 
 def initialize_hearts(suit, straight)
-  i = 0
+  index = 0
   hearts = []
-  while i < straight.size
-    hearts[i] = suit[0] + straight[i]
-    i += 1
+  while index < straight.size
+    hearts[index] = suit[0] + straight[index]
+    index += 1
   end
   hearts
 end
 
 def initialize_clubs(suit, straight)
-  i = 0
+  index = 0
   clubs = []
-  while i < straight.size
-    clubs[i] = suit[1] + straight[i]
-    i += 1
+  while index < straight.size
+    clubs[index] = suit[1] + straight[index]
+    index += 1
   end
   clubs
 end
 
 def initialize_diamonds(suit, straight)
-  i = 0
+  index = 0
   diamonds = []
-  while i < straight.size
-    diamonds[i] = suit[2] + straight[i]
-    i += 1
+  while index < straight.size
+    diamonds[index] = suit[2] + straight[index]
+    index += 1
   end
   diamonds
 end
 
 def initialize_spades(suit, straight)
-  i = 0
+  index = 0
   spades = []
-  while i < straight.size
-    spades[i] = suit[3] + straight[i]
-    i += 1
+  while index < straight.size
+    spades[index] = suit[3] + straight[index]
+    index += 1
   end
   spades
 end
 
 def initialize_deck(suit, straight)
-  deck = initialize_hearts(suit, straight) + initialize_clubs(suit, straight) +
-         initialize_diamonds(suit, straight) + initialize_spades(suit, straight)
-  deck
+  initialize_hearts(suit, straight) + initialize_clubs(suit, straight) +
+    initialize_diamonds(suit, straight) + initialize_spades(suit, straight)
 end
 
 def prompt(msg)
@@ -63,8 +62,8 @@ end
 
 def display_card_player(cards)
   prompt "You have: "
-  cards.size.times do |i|
-    prompt("- #{cards[i][1]} of #{cards[i][0]}")
+  cards.size.times do |index|
+    prompt("- #{cards[index][1]} of #{cards[index][0]}")
   end
 end
 
@@ -76,8 +75,8 @@ end
 
 def display_card_dealer(cards)
   prompt("Dealer has: ")
-  cards.size.times do |i|
-    prompt("- #{cards[i][1]} of #{cards[i][0]}")
+  cards.size.times do |index|
+    prompt("- #{cards[index][1]} of #{cards[index][0]}")
   end
 end
 
@@ -98,39 +97,39 @@ end
 
 def card_count(hand)
   if contains_ace?(hand) == false
-    total = basic_total(hand)
+    basic_total(hand)
   elsif basic_total(hand) + 10 <= 21
-    total = basic_total(hand) + 10
+    basic_total(hand) + 10
   else
-    total = basic_total(hand)
+    basic_total(hand)
   end
-  total
+end
+
+def display_card_count(current_player, hand)
+  prompt("#{current_player}'s current count is: #{card_count(hand)}")
 end
 
 def busted?(hand)
-  if card_count(hand) > 21
-    return true
-  else
-    return false
-  end
+  card_count(hand) > 21
 end
 
 def acceptable_answer?(user_input)
-  if user_input == "stay" || user_input == "hit"
-    return true
-  else
-    return false
-  end
+  user_input == "stay" || user_input == "hit"
 end
 
-def play_again?
-  prompt("Would you like to play again ('y'/'n')")
-  answer = gets.chomp.downcase
-  if answer.start_with?('y')
-    return true
-  else
-    return false
+def play_again_question
+  answer = ' '
+  loop do
+    prompt("Would you like to play again? ('yes' or 'no')")
+    answer = gets.chomp
+    answer.downcase!
+    answer == "yes" || answer == "no" ? break : next
   end
+  answer
+end
+
+def play_again?(answer)
+  answer == "yes"
 end
 
 def difference_from_21(hand)
@@ -138,19 +137,11 @@ def difference_from_21(hand)
 end
 
 def dealer_won?(player_cards, dealer_cards)
-  if difference_from_21(player_cards) > difference_from_21(dealer_cards)
-    return true
-  else
-    return false
-  end
+  difference_from_21(player_cards) > difference_from_21(dealer_cards)
 end
 
 def tie?(player_cards, dealers_cards)
-  if difference_from_21(player_cards) == difference_from_21(dealers_cards)
-    return true
-  else
-    return false
-  end
+  difference_from_21(player_cards) == difference_from_21(dealers_cards)
 end
 
 loop do
@@ -166,18 +157,20 @@ loop do
   player_cards += [deal_card(deck)]
   player_cards += [deal_card(deck)]
   display_card_player(player_cards)
+  display_card_count("Player", player_cards)
 
   answer = nil
   loop do
     prompt("hit or stay?")
     answer = gets.chomp
-    answer.downcase
-    next if acceptable_answer?(answer) == false
+    answer.downcase!
+    next unless acceptable_answer?(answer)
     if answer == "hit"
       puts " "
       player_cards += [deal_card(deck)]
       display_card_dealer_initial(dealers_cards)
       display_card_player(player_cards)
+      display_card_count("Player", player_cards)
     end
     break if answer == 'stay' || busted?(player_cards)
   end
@@ -186,7 +179,7 @@ loop do
   if busted?(player_cards)
     prompt("Player Busted!!!")
     prompt("Dealer wins! You lost this round!")
-    if play_again? == true
+    if play_again?(play_again_question)
       3.times { puts " " }
       next
     else
@@ -199,7 +192,9 @@ loop do
 
   loop do
     display_card_player(player_cards)
+    display_card_count("Player", player_cards)
     display_card_dealer(dealers_cards)
+    display_card_count("Dealer", dealers_cards)
     if busted?(dealers_cards)
       prompt("Dealer Busted!!!")
       prompt("Player won!!!")
@@ -221,7 +216,7 @@ loop do
     end
   end
 
-  if play_again? == true
+  if play_again?(play_again_question)
     3.times { puts " " }
     next
   else
